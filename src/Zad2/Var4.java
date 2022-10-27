7ef119871da878469653fa66c5f2641ec5b72f21
Sivtsev-Ivan <100548579+Sivtsev-Ivan@users.noreply.github.com>
2022-10-27 21:28:04 +0900

    Add files via upload

---
+++ src/Zad2/Var4.java
@@ -0,0 +1,35 @@
+package Zad2;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Var4 {
+    public static void main(String[] args) {
+        try {
+            File f1 = new File("C:\\Games\\input.txt");
+            DataInputStream rd = new DataInputStream(new FileInputStream(f1.getAbsolutePath()));
+            File f2 = new File("C:\\Games\\otput.txt");
+            f2.createNewFile();
+            DataOutputStream wr = new DataOutputStream(new FileOutputStream(f2.getAbsolutePath()));
+
+            int count = 0;
+            try {
+                while (true) {
+                    float number = rd.readFloat();
+                    if (number < 0) {
+                        wr.writeFloat(number);
+                        count++;
+                    }
+                }
+            } catch (EOFException e) {
+                wr.writeInt(count);
+                wr.flush();
+                wr.close();
+                rd.close();
+            }
+        }
+        catch (Exception e) {
+
+        }
+    }
+}
